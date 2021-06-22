ee0d8a1d74dcfead33add4a89a5485bfd2a7ad52
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-06-23 02:24:14 +0300

    Add model to the app

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const passport = require('passport');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 const connectDB = require('./config/db');
 
 //Load config
 
 dotenv.config({path: './config/config.env'});
+
+// Passport
+
+require('./config/passport')(passport);
 
 //Connect to Database
 connectDB();
@@ -22,6 +28,18 @@ if (process.env.NODE_ENV === 'development') {
 // handlebars
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
+
+// Sessions
+app.use(session({
+  secret: 'tanzania',
+  resave: false,
+  saveUninitialized: false
+}))
+
+// middleware
+
+app.use(passport.initialize());
+app.request(passport.session());
 
 // Static
 app.use(express.static(path.join(__dirname, 'public')));
