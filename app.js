bdf7c4ba21ecea2378938242089d4a601124d894
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-06-23 01:12:33 +0300

    Modify app.js file

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 const connectDB = require('./config/db');
 
 //Load config
 
 dotenv.config({path: './config/config.env'});
 
+//Connect to Database
 connectDB();
 
 const app = express();
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 const PORT = process.env.PORT || 5000;
 
